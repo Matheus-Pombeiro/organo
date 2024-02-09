3e26ef8516bd4b29733c53b86f9de6f1a168851a
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-02-09 18:39:39 -0300

    FIX: rename the file

---
+++ src/components/banner/index.js
@@ -0,0 +1,11 @@
+import "./Banner.css";
+
+const Banner = () => {
+    return (
+        <header className="banner">
+            <img src="/images/banner.png" alt="Page's main banner" />
+        </header>
+    )
+};
+
+export default Banner;
