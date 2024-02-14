3f59ed30f9230a7907cac337779c4c5471b6f84d
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-02-14 20:45:33 -0300

    ADD: clean the input fields

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -14,13 +14,16 @@ const Formulario = (props) => {
 
     const aoSalvar = (e) => {
         e.preventDefault();
-        console.log(`Form foi submetido => ${nome}, ${cargo}, ${imagem}, ${time}`)
         props.aoColaboradorCadastrado({
             nome,
             cargo,
             imagem,
             time
         });
+        setNome("");
+        setCargo("");
+        setImagem("");
+        setTime("");
     };
 
     return (
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -4,17 +4,18 @@ import Colaborador from "../Colaborador";
 
 const Time = (props) => {
     return (
-        <section className="time" style={{ backgroundColor: props.corSecundaria }}>
-            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-            <div className="colaboradores">
-                {props.colaboradores.map(colaborador => <Colaborador
-                    key={colaborador.nome}
-                    nome={colaborador.nome}
-                    cargo={colaborador.cargo}
-                    imagem={colaborador.imagem}
-                />)}
-            </div>
-        </section>
+        (props.colaboradores.length > 0) ? 
+            <section className="time" style={{ backgroundColor: props.corSecundaria }}>
+                <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+                <div className="colaboradores">
+                    {props.colaboradores.map(colaborador => <Colaborador
+                        nome={colaborador.nome}
+                        cargo={colaborador.cargo}
+                        imagem={colaborador.imagem}
+                    />)}
+                </div>
+            </section>
+        : ""
     )
 };
 
